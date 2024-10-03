775607e4b860b6a684562fb2f116265d3ac80b15
ronaldo1223 <ronaldovasconcelo70@gmail.com>
2024-10-03 19:28:22 -0300

    atualização das aulas
    estou aprendendo sobre git e github

---
+++ calculadora.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+def soma (x,y):
+    """param x=primeiro número
+    param y=segundo número
+    faz a soma entre x e y
+    ex:soma(1,2) 3
+    """
+    return x+y
+def sub (x,y):
+    return x-y
+
+def mult(x,y):
+    return x*y
+
+def div(x,y):
+    return x/y
+
+
+# In[ ]:
+
+
+
+
---
+++ matematica/geometria.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+def areacirculo(r):
+    import math
+    return math.pi* math.pow(r,2)
+
